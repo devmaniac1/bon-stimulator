c8f0b89b8387c76ef7d928a62ac267443a390e9e
Ashfaq Farleen <139785309+devmaniac1@users.noreply.github.com>
2025-05-01 19:22:21 +0530

    Update slider.tsx

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -26,10 +26,10 @@ const Slider = React.forwardRef<
       {...props}
     >
       <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-slate-200">
-        <SliderPrimitive.Range className={`absolute h-full ${colorClass}`} />
+        <SliderPrimitive.Range className={`absolute h-full bg-slate-500`} />
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb
-        className={`block h-5 w-5 rounded-full border-2 border-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${colorClass} ${thumbClassName}`}
+        className={`block h-5 w-5 rounded-full border-2 border-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-slate-500 ${thumbClassName}`}
         //   colorClass,
         //   thumbClassName
         // )}
